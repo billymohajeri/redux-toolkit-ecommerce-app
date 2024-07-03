978a41a4c9b7fc2be6286cdc69ba189d0f3b3f72
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-03 15:43:02 +0300

    Configure slice to handle cases

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,7 +1,15 @@
-import { useSelector } from 'react-redux'
-import { RootState } from '../store/store'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '../store/store'
+import { useEffect } from 'react'
+import { fetchData } from '../features/products/productSlice'
 
 const Products = () => {
+  const dispatch: AppDispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchData())
+  }, [dispatch])
+
   const { products, loading, error } = useSelector((state: RootState) => state.productReducer)
   console.log(products)
   return <div>List all the products here</div>
--- src/features/products/productSlice.ts
+++ src/features/products/productSlice.ts
@@ -8,12 +8,32 @@ export const fetchData = createAsyncThunk('products/fetchData', async () => {
     const response = await fetch('https://dummyjson.com/products')
     const data = await response.json()
     console.log(data)
+    return data
   } catch (error) {
     console.log(error)
     console.error(error)
   }
 })
 
-const productsSlice = createSlice({ name: 'products', initialState: initialState, reducers: {} })
+const productsSlice = createSlice({
+  name: 'products',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.loading = false
+        state.products = action.payload
+      })
+      .addCase(fetchData.rejected, (state) => {
+        state.loading = false
+        state.error = 'An error occurred'
+      })
+  }
+})
 
 export default productsSlice.reducer
